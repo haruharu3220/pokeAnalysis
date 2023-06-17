060840a7eeee131906f0daedff7fcaaff6f80938
Hayato <hayato.314.nico@gmail.com>
2023-06-18 01:04:19 +0900

    questionsページへのリンクを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="6">
-        <v-card>
-          <v-card-title>Hello, Vuetify!</v-card-title>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="6">
+          <v-card>
+            <v-card-title>Hello, Vuetify!</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <NuxtLink to="/questions">リンクだよ</NuxtLink>
+  </div>
 </template>
 
 <script lang="js">
