2579ffaf82dc6a020fede5627fd7bc55e85dc681
Hayato <hayato.314.nico@gmail.com>
2023-06-18 00:46:25 +0900

    vuetifyを途中から入れる

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,28 +4,24 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'pokeAnalysis',
+    title: "pokeAnalysis",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,18 +29,30 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
+    "@nuxtjs/vuetify", //  追加
   ],
+  //  追加
+  vuetify: {
+    theme: {
+      primary: "#03a9f4",
+      secondary: "#607d8b",
+      accent: "#9c27b0",
+      error: "#f44336",
+      warning: "#ffc107",
+      info: "#009688",
+      success: "#4caf50",
+    },
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/chakra
-    '@chakra-ui/nuxt',
+    "@chakra-ui/nuxt",
     // https://go.nuxtjs.dev/emotion
-    '@nuxtjs/emotion',
+    "@nuxtjs/emotion",
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
