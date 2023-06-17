92d5d8ec7cccc2cd6a38e45bfeecaeca836f7710
Hayato <hayato.314.nico@gmail.com>
2023-06-18 01:30:41 +0900

    TOPページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="6">
-          <v-card>
-            <v-card-title>Hello, Vuetify!</v-card-title>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-sheet
+      elevation="12"
+      max-width="600"
+      rounded="lg"
+      width="100%"
+      class="pa-4 text-center mx-auto mt-4"
+    >
+      <h2 class="text-h5 mb-6">ポケモン自己診断</h2>
 
-    <NuxtLink to="/questions">リンクだよ</NuxtLink>
+      <p class="mb-4 text-medium-emphasis text-body-2">
+        ポケモン自己診断っぽいものを作ってみました！
+        <br />
+        遊んでね！
+      </p>
+      <!-- 罫線(不要なので外す)
+      <v-divider class="mb-4"></v-divider> -->
+
+      <div class="text-center">
+        <v-btn
+          class="text-none"
+          color="yellow"
+          rounded
+          variant="flat"
+          width="200"
+          to="/questions"
+        >
+          　診断をはじめる
+        </v-btn>
+      </div>
+    </v-sheet>
   </div>
 </template>
-
+<style>
+.custom-link {
+  text-decoration: none;
+  color: black;
+}
+</style>
 <script lang="js">
 
 export default {
